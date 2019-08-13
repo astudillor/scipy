fdeabed33388e75cc3387740f2ec4f231d9da87e
rastudilloreng <r.a.astudillo@tudelft.nl>
2019-08-13 17:27:07 +0200

    start addind idrs

--- scipy/sparse/linalg/isolve/__init__.py
+++ scipy/sparse/linalg/isolve/__init__.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File  : __init__.py
+# Author: Reinaldo Astudillo <r.a.astudillo@tudelft.nl>
 "Iterative Solvers for Sparse Linear Systems"
 
 from __future__ import division, print_function, absolute_import
@@ -9,6 +13,7 @@ from .lgmres import lgmres
 from .lsqr import lsqr
 from .lsmr import lsmr
 from ._gcrotmk import gcrotmk
+from .idrs import idrs
 
 __all__ = [s for s in dir() if not s.startswith('_')]
 
